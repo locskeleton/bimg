7f5e270016f0b008b89c9b40fc6988907a1f27e4
Locskeleton <locnguyen.it@gmail.com>
2019-01-15 16:54:32 +0700

    Write more log

--- vips.go
+++ vips.go
@@ -727,10 +727,10 @@ func vipsText(image *C.VipsImage, to TextOption) (*C.VipsImage, error) {
 
 	defer C.free(unsafe.Pointer(text))
 	defer C.free(unsafe.Pointer(font))
-	var textOptions = (*C.VipsTextOptions)(unsafe.Pointer(&opts))
-	
+	textOptions := (*C.VipsTextOptions)(unsafe.Pointer(&opts))
+
 	fmt.Println("Prepare call C.vips_text_bridge function")
-	err := C.vips_text_bridge(image, &out, textOptions)
+	//err := C.vips_text_bridge(image, &out, textOptions)
 	if err != 0 {
 		return nil, catchVipsError()
 	}
